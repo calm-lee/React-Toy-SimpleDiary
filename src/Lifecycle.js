9e79aa10c0193c6df289f756ec3843cf442939c7
Calm Lee <calm_lee@naver.com>
2022-04-10 23:09:07 +0900

    mount, update, unmount by useEffect

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -12,6 +12,18 @@ const Lifecycle = () => {
     console.log("Update!");
   });
 
+  useEffect(() => {
+    console.log(`count is updated : ${count}`);
+    if (count > 5) {
+      alert("count가 5를 넘었습니다. 따라서 1로 초기화합니다.");
+      setCount(1);
+    }
+  }, [count]);
+
+  useEffect(() => {
+    console.log(`text is updeated : ${text}`);
+  }, [text]);
+
   return (
     <div style={{ padding: 20 }}>
       <div>
